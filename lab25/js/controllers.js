0cf71c3ccfc388314a78e4f3a68bb136cbf47024
Alejandro Romero <alejo8591@gmail.com>
2015-04-29 13:50:07 -0500

    Update repo structure :neckbeard:

--- lab25/js/controllers.js
+++
@@ -1,9 +0,0 @@
-angular.module('initial_spi.controllers', [])
-
-.controller('mobileController', function($scope){
-	$scope.mobile_message = 'Algún Mensaje';
-})
-
-.controller('smartphoneController', function($scope){
-	$scope.smartphone_message = 'Otro Mensaje';
-});
