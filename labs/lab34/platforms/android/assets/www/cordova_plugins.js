7f83bf2287d04cefd9a756c4da019515545a0abb
Alejandro Romero <alejo8591@gmail.com>
2015-05-12 15:48:03 -0500

    Update `lab33` and `lab34`

--- labs/lab34/platforms/android/assets/www/cordova_plugins.js
+++ labs/lab34/platforms/android/assets/www/cordova_plugins.js
@@ -4,12 +4,27 @@ module.exports = [
         "file": "plugins/cordova-plugin-whitelist/whitelist.js",
         "id": "cordova-plugin-whitelist.whitelist",
         "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-device-motion/www/Acceleration.js",
+        "id": "cordova-plugin-device-motion.Acceleration",
+        "clobbers": [
+            "Acceleration"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device-motion/www/accelerometer.js",
+        "id": "cordova-plugin-device-motion.accelerometer",
+        "clobbers": [
+            "navigator.accelerometer"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.0.0"
+    "cordova-plugin-whitelist": "1.0.0",
+    "cordova-plugin-device-motion": "1.1.0"
 }
 // BOTTOM OF METADATA
 });
--- labs/lab34/platforms/firefoxos/www/cordova_plugins.js
+++ labs/lab34/platforms/firefoxos/www/cordova_plugins.js
@@ -1,9 +1,30 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-device-motion/www/Acceleration.js",
+        "id": "cordova-plugin-device-motion.Acceleration",
+        "clobbers": [
+            "Acceleration"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device-motion/www/accelerometer.js",
+        "id": "cordova-plugin-device-motion.accelerometer",
+        "clobbers": [
+            "navigator.accelerometer"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device-motion/src/firefoxos/accelerometer.js",
+        "id": "cordova-plugin-device-motion.accelerometer-impl",
+        "runs": true
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.0.0"
+    "cordova-plugin-whitelist": "1.0.0",
+    "cordova-plugin-device-motion": "1.1.0"
 }
 // BOTTOM OF METADATA
 });
